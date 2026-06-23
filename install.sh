3377f9617c3bb1856c0def818e7ca35530e45d64
K Shiva <147469995+KkShiva@users.noreply.github.com>
2026-06-23 20:27:05 +0530

    Refactor install.sh for improved package handling
    Updated installation script to streamline package installation and setup for Copyparty.

--- install.sh
+++ install.sh
@@ -1,30 +1,32 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-set -e
-
 echo "Updating packages..."
 pkg update -y
 
-echo "Installing dependencies..."
-pkg install -y python net-tools termux-api
-
-echo "Grant storage access..."
-termux-setup-storage
+echo "Installing Python..."
+pkg install -y python
+pkg install net-tools
 
 echo "Installing Copyparty..."
-python -m pip install --upgrade pip
-python -m pip install --upgrade copyparty
+pip install --upgrade pip
+pip install --upgrade copyparty
 
-mkdir -p ~/PhoneToServer
 
-curl -L 
-https://raw.githubusercontent.com/KkShiva/PhoneToServer-termux/main/app.py 
--o ~/PhoneToServer/app.py
 
-chmod +x ~/PhoneToServer/app.py
 
-echo ""
-echo "Installation completed."
-echo ""
-echo "Run:"
-echo "python ~/PhoneToServer/app.py"
+mkdir -p ~/.config/copyparty
+
+cat > ~/.config/copyparty/start.sh << 'EOF'
+#!/data/data/com.termux/files/usr/bin/bash
+
+copyparty -p 8080 -v ~/storage/shared::r
+EOF
+
+chmod +x ~/.config/copyparty/start.sh
+
+
+
+
+ifconfig
+copyparty -p 8080 -v ~/storage/shared::r
+
