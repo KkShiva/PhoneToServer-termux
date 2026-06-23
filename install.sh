bcb1a1881cb7c3cc39ef8aab4841a1bf01c6bde5
K Shiva <147469995+KkShiva@users.noreply.github.com>
2026-06-23 20:01:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
+set -e
+
+APPDIR="$HOME/copyparty-android"
+
 pkg update -y
-pkg install -y python net-tools
+pkg install -y python wget unzip net-tools
+
+rm -rf "$APPDIR"
+
+wget -O master.zip 
+https://github.com/YOURUSER/copyparty-android/archive/refs/heads/main.zip
+
+unzip master.zip
+
+mv copyparty-android-main "$APPDIR"
+
+cd "$APPDIR"
+
+pip install --upgrade pip
 pip install -r requirements.txt
+
+echo ""
+echo "Starting Web UI..."
+echo ""
+
 python server.py
