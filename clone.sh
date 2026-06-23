744fbfcc6f86e4dc2f8761799d4ed116ca1d1b27
K Shiva <147469995+KkShiva@users.noreply.github.com>
2026-06-24 00:58:58 +0530

    Update download link in clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,4 @@
 wget -O master.zip
-https://github.com/YOURUSER/copyparty-android/archive/refs/heads/main.zip
+https://github.com/KkShiva/PhoneToServer-termux/archive/refs/heads/main.zip
 
 unzip master.zip
