6bc4cbe7fa96a3449faf7afa447293c19da54ad4
K Shiva <147469995+KkShiva@users.noreply.github.com>
2026-06-23 21:24:06 +0530

    Delete static directory

--- static/app.js
+++
@@ -1,6 +0,0 @@
-async function load(){let r=await fetch('/api/info');let j=await r.json();window.ip=j.ip;upd();}
-function upd(){document.getElementById('url').innerText='http://'+window.ip+':'+document.getElementById('port').value;}
-document.addEventListener('input',upd);
-async function startSrv(){await fetch('/api/start',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({folder:folder.value,port:port.value})});}
-async function stopSrv(){await fetch('/api/stop',{method:'POST'});}
-load();
