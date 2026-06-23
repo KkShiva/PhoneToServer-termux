b8593df6036bdb5cc256162f8464acda78970705
K Shiva <147469995+KkShiva@users.noreply.github.com>
2026-06-23 21:50:22 +0530

    Refactor install.sh for Copyparty setup
    Removed unnecessary installation steps and added configuration for Copyparty.

--- install.sh
+++ install.sh
@@ -3,44 +3,29 @@
 echo "Updating packages..."
 pkg update -y
 
-echo "Installing requirements..."
-pkg install -y git python unzip
+echo "Installing Python..."
+pkg install -y python
+pkg install net-tools
 
-WORKDIR="$HOME/PhoneToServer-termux"
-
-echo "Cleaning old files..."
-rm -rf "$WORKDIR"
-
-echo "Cloning repository..."
-git clone https://github.com/KkShiva/PhoneToServer-termux.git "$WORKDIR"
-
-echo
-echo "======================================"
-echo "Installation Complete"
-echo "======================================"
-echo
-
-echo "Project location:"
-echo "$WORKDIR"
-echo
-
-echo "Files:"
-ls -lah "$WORKDIR"
-
-echo
-echo "To run manually:"
-echo
-
-echo "cd $WORKDIR"
-echo "python setup_copyparty.py"
-echo
-
-cd ~/PhoneToServer-termux
+echo "Installing Copyparty..."
+pip install --upgrade pip
+pip install --upgrade copyparty
 
 
 
 
-ls
+mkdir -p ~/.config/copyparty
+
+cat > ~/.config/copyparty/start.sh << 'EOF'
+#!/data/data/com.termux/files/usr/bin/bash
+
+copyparty -p 8080 -v ~/storage/shared::r
+EOF
+
+chmod +x ~/.config/copyparty/start.sh
 
 
 
+
+ifconfig
+copyparty -p 8080 -v ~/storage/shared::r
