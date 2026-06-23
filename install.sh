2eb228ab1f1d0eae5225ded64fe97b9cdce65040
K Shiva <147469995+KkShiva@users.noreply.github.com>
2026-06-23 20:50:32 +0530

    Modify install.sh for package updates and Copyparty
    Updated package installation and added Copyparty setup steps.

--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+echo "Updating packages..."
 pkg update -y
-pkg install -y python net-tools
 
+echo "Installing requirements..."
+pkg install -y python unzip wget
+
+echo "Installing Copyparty..."
 pip install --upgrade pip
 pip install --upgrade copyparty
 
-mkdir -p ~/.config/copyparty
+WORKDIR="$HOME/PhoneToServer-termux"
+
+rm -rf "$WORKDIR"
+mkdir -p "$WORKDIR"
+
+cd "$HOME" || exit 1
+
+wget -O PhoneToServer-termux.zip 
+https://github.com/KkShiva/PhoneToServer-termux/archive/refs/heads/main.zip
+
+unzip -o PhoneToServer-termux.zip
+
+cd PhoneToServer-termux-main || exit 1
 
 python setup_copyparty.py
